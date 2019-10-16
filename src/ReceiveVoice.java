0599e7857008a4cfc7396f31bb8134a1d9b70134
irfanm96 <e15138@ce.pdn.ac.lk>
2019-10-16 21:38:15 +0530

    added files

---
+++ src/ReceiveVoice.java
@@ -0,0 +1,87 @@
+import java.net.*;
+
+public class ReceiveVoice extends Main {
+
+    private final int packetSize = 1000;
+    private int port;
+    private MulticastSocket socket;
+    private InetAddress host;
+
+    @Override
+    public void run() {
+
+        try {
+            this.socket = new MulticastSocket(this.port);
+
+            //avoid hearing my voice again
+            this.socket.setLoopbackMode(true);//avoid loop back
+
+            //used for windows environments ,it enables the multicast
+            this.socket.setBroadcast(true);
+            //join the multicast group
+            this.socket.joinGroup(this.host);
+
+            // Create a packet
+            DatagramPacket packet = new DatagramPacket(new byte[this.packetSize], (this.packetSize));
+
+            //setup audio outputs, from parent class
+            this.playAudio();
+            for (; ; ) {
+//                System.out.print("hi");
+                try {
+
+//                    System.out.println("receiving");
+
+                    this.socket.receive(packet);
+
+                    // Print the packet
+                    this.getSourceDataLine().write(packet.getData(), 0, this.packetSize); //playing the audio
+
+                    packet.setLength(this.packetSize);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.socket.close();
+        }
+    }
+
+    public ReceiveVoice(InetAddress host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+
+        // Check the whether the arguments are given
+        if (args.length != 1) {
+            System.out.println("Multicast ip Required");
+            return;
+        }
+
+        final int port = 3575;
+
+        SendVoice sendVoice;
+        ReceiveVoice receiveVoice;
+
+        try {
+
+            //create the thread for sending packets
+            sendVoice = new SendVoice(InetAddress.getByName(args[0]), port);
+            sendVoice.start(); // start the thread
+
+            //create thread for receiving packets
+            receiveVoice = new ReceiveVoice(InetAddress.getByName(args[0]), port);
+            receiveVoice.start();//start receiving packets
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/SendVoice.java
@@ -0,0 +1,63 @@
+import java.net.*;
+import java.io.IOException;
+
+
+public class SendVoice extends Main {
+
+    private final int packetSize = 1000;
+    private int port;
+    private InetAddress host;
+    private MulticastSocket socket = null;
+    private byte buffer[] = new byte[this.packetSize];
+    
+    public SendVoice(InetAddress host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    private void send() {
+        try {
+            int count;
+            for (;;) {
+                System.out.print("");
+                count = this.getTargetDataLine().read(this.buffer, 0, this.buffer.length);  //capture sound into buffer
+                if (count > 0) {
+//                    System.out.println("sending audio");
+                    // Construct the datagram packet
+                    DatagramPacket packet = new DatagramPacket(this.buffer, this.buffer.length, this.host, this.port);
+                    // Send the packet
+                    this.socket.send(packet);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void run() {
+        try {
+            this.socket = new MulticastSocket(this.port);
+
+            //avoid hearing my voice again 
+            this.socket.setLoopbackMode(true);//avoid loop back 
+
+            //used for windows environments ,it enables the multicast 
+            this.socket.setBroadcast(true);
+            //join the multicast group
+            this.socket.joinGroup(this.host);
+
+            //setup the audio input and output,function is in the parent class
+            this.captureAudio();
+
+            //start sending the packets
+            this.send();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.socket.close();
+        }
+    }
+
+
+}
