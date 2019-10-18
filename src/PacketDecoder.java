648a48808ef6e663bbf6f93987d892ff38ee78b0
ris <github@ris.fi>
2019-10-19 02:52:54 +0530

    Added enc/dec.

--- src/PacketDecoder.java
+++ src/PacketDecoder.java
@@ -2,12 +2,22 @@ import java.nio.*;
 import java.io.*;
 import java.util.*;
 import java.lang.*;
+import java.security.Security;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class PacketDecoder{
 	byte[] buffer;
 	int seq;
 	int user;
 	PacketDecoder(byte[] buffer){
+		byte[] seqBytes = new byte[4];
+		user = ByteBuffer.wrap(Arrays.copyOf(buffer, 4)).getInt();
+		System.arraycopy(buffer, 4, seqBytes, 0, 4);
+		seq = ByteBuffer.wrap(seqBytes).getInt();
+		System.arraycopy(buffer, 8, this.buffer, 0, buffer.length-8);
+	}
+	PacketDecoder(byte[] buffer, byte[] key){
 		byte[] seqBytes = new byte[4];
 		user = ByteBuffer.wrap(Arrays.copyOf(buffer, 4)).getInt();
 		System.arraycopy(buffer, 4, seqBytes, 0, 4);
--- src/PacketEncoder.java
+++ src/PacketEncoder.java
@@ -1,11 +1,25 @@
 import java.nio.*;
 import java.io.*;
+import java.security.Security;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class PacketEncoder{
 	byte[] buffer;
 	byte[] seqBytes;
 	byte[] userBytes;
 	PacketEncoder(int user, int seq, byte[] buffer){
+		userBytes = ByteBuffer.allocate(4).putInt(user).array();
+		seqBytes = ByteBuffer.allocate(4).putInt(seq).array();
+		ByteArrayOutputStream BOS = new ByteArrayOutputStream();
+		try{
+		BOS.write(userBytes);
+		BOS.write(seqBytes);
+		BOS.write(buffer);
+		} catch (IOException E) {}
+		this.buffer=BOS.toByteArray();
+	}
+	PacketEncoder(int user, int seq, byte[] buffer, byte[] key){
 		userBytes = ByteBuffer.allocate(4).putInt(user).array();
 		seqBytes = ByteBuffer.allocate(4).putInt(seq).array();
 		ByteArrayOutputStream BOS = new ByteArrayOutputStream();
