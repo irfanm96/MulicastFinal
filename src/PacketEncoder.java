7393c4c65980acde684b914494d85a0871a728ce
irfanm96 <e15138@ce.pdn.ac.lk>
2019-10-20 12:40:15 +0530

    emergency updates

--- src/PacketEncoder.java
+++ src/PacketEncoder.java
@@ -11,19 +11,6 @@ public class PacketEncoder {
     byte[] userBytes;
     private static final String ENCRYPTION_ALGORITHM = "ARCFOUR";
 
-    PacketEncoder(int user, int seq, byte[] buffer) {
-        userBytes = ByteBuffer.allocate(4).putInt(user).array();
-        seqBytes = ByteBuffer.allocate(4).putInt(seq).array();
-        ByteArrayOutputStream BOS = new ByteArrayOutputStream();
-        try {
-            BOS.write(userBytes);
-            BOS.write(seqBytes);
-            BOS.write(buffer);
-        } catch (IOException E) {
-        }
-        this.buffer = BOS.toByteArray();
-    }
-
     PacketEncoder(int user, int seq, byte[] buffer, byte[] key) {
         try {
             //initialize secret key for the encryption algorithm provided
